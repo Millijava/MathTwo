75ac22ebf537aeaf4c9d8ccaa1eab8c6251e5541
GBTC441003UR <GBTC441003UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-09-04 16:53:32 -0400

    Exercise3

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int mynumber = 2 ;
+
+        double myothernumber = 1.7938 ;
+        System.out.println();
+        System.out.println(mynumber);
+        System.out.println(myothernumber);
+//        System.out.print( "The value " + 2 + " is the largest value ");
+    }
+}
